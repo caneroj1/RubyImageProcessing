9f2bcdfc1a85c7af179125d198288dc67bcdab0a
caneroj1 <caneroj1@tcnj.edu>
2014-04-21 01:36:10 -0400

    Added a rakefile that will allow you to keep track of the main images you are working with. allows you to easily delete everything that isnt a main image.

---
+++ Rakefile.rb
@@ -0,0 +1,46 @@
+nameHash = Hash.new()
+processing = ["Grayed.png", "SobelFilter.png", "Blurred.png", "Gauss.png", "Sharpened.png"]
+
+directory "info"
+file "info/pictures.txt" => "info"
+
+task :create, [:name] => "info/pictures.txt" do |t, args|
+    if !args.name.nil? then
+    File.open("info/pictures.txt", "a") do |f|
+      puts "info/pictures.txt ----> wrote #{args.name}"
+      f.write("#{args.name}\n")
+      if !args.extras.nil? then
+        args.extras.each do |e|
+          puts "info/pictures.txt ----> wrote #{e}"
+          f.write("#{e}\n")
+        end
+      end
+    end
+  end
+end
+
+
+task :clean do
+  # create array of file names from pictures.txt
+  names = Array.new()
+  File.open("info/pictures.txt", "r") do |f|
+    f.each_line do |l|
+      names.push(l[0..-6])
+    end
+  end
+  
+  # try to remove files with all possible extensions created through processing
+  names.each do |i|
+    processing.each do |j|
+      begin
+        File.delete("pictures/#{i}"+"#{j}")
+      rescue => e
+        puts "Could not delete: #{e.message}"
+      end
+    end
+  end  
+  
+  # delete pictures.txt
+  puts "Deleting ----> info/pictures.txt"
+  File.delete("info/pictures.txt")
+end
