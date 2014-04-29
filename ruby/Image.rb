5c3b0a221b8966122122f1a8bddd5b91cacf1006
caneroj1 <caneroj1@tcnj.edu>
2014-04-29 16:43:28 -0400

    changed some of the code to use more idiomatic ruby syntax.

--- ruby/Image.rb
+++ ruby/Image.rb
@@ -75,9 +75,7 @@ class Image
   # if there is no path, it defaults to the directory the current picture is located
   
   def convertToGrayscale(name = nil, path = nil)
-    if name.nil? then
-      name = @pictureName.gsub('.png', 'Grayed.png')
-    end
+    name ||= @pictureName.gsub('.png', 'Grayed.png')
     grayImage = @picture.grayscale
     path.nil? ? grayImage.save(File.join(@picturePath, name)) : grayImage.save(File.join(path, name))
   end
@@ -93,13 +91,8 @@ class Image
   # the results of each matrix are then put into the distance equation which then becomes that pixel's color
   
   def sobelFilter(name = nil, pathForImage = nil, pathForSave = nil)
-    if name.nil? then
-      name = @pictureName.gsub('.png', 'SobelFilter.png')
-    end
-    
-    if pathForImage.nil? then
-      pathForImage = File.join(@picturePath, @pictureName.gsub('.png', 'Grayed.png'))
-    end
+    name ||= @pictureName.gsub('.png', 'SobelFilter.png')
+    pathForImage ||= File.join(@picturePath, @pictureName.gsub('.png', 'Grayed.png'))
     
     sobelFilterX = [[1, 2, 1], [0, 0, 0], [-1, -2, -1]]
     sobelFilterY = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
@@ -128,13 +121,8 @@ class Image
   # significantly faster. if one does not want an image as blurred as with the gaussian algorithm, this might be desirable.
   
   def blur(name = nil, pathForImage = nil, pathForSave = nil)
-    if name.nil? then
-      name = @pictureName.gsub('.png', 'Blurred.png')
-    end
-    
-    if pathForImage.nil? then
-      pathForImage = File.join(@picturePath, @pictureName)
-    end
+    name ||= @pictureName.gsub('.png', 'Blurred.png')
+    pathForImage ||= File.join(@picturePath, @pictureName)
     
     blurFilter = [[1/16.0, 2/16.0, 1/16.0], [2/16.0, 4/16.0, 2/16.0], [1/16.0, 2/16.0, 1/16.0]]
     height = @height-2
@@ -158,13 +146,8 @@ class Image
   # since this is a 5x5 matrix, it is computationally more intense.
   
   def gaussianBlur(name = nil, pathForImage = nil, pathForSave = nil)
-    if name.nil? then
-      name = @pictureName.gsub('.png', 'Gauss.png')
-    end
-    
-    if pathForImage.nil? then
-      pathForImage = File.join(@picturePath, @pictureName)
-    end
+    name ||= @pictureName.gsub('.png', 'Gauss.png')
+    pathForImage ||= File.join(@picturePath, @pictureName)
     
     blurFilter = 
     [
@@ -179,21 +162,21 @@ class Image
     width = @width-3
     blur = ChunkyPNG::Image.from_file(pathForImage)
 
-    t1 = Time.now
+    # t1 = Time.now
     # this section takes approximately 4.2482 seconds. The Lenna image is 220x220 pixels, so:
     # 220^2 = 48400. 4.2482 / 48400 -> each pixel takes approximately 0.0877 milliseconds to be processed.
 
     for j in 2..(height)
       for i in 2..(width)
-        pixel = cpvFilter5Optimize(blurFilter, @picture, i, j, false)
+        pixel = calculatePixelValueWithFilter5(blurFilter, @picture, i, j, false)
         blur[i, j] = ChunkyPNG::Color.rgb(pixel[0].to_i, pixel[1].to_i, pixel[2].to_i)
       end
     end
 
-    t2 = Time.now
-    "%.9f" % t1.to_f
-    "%.9f" % t2.to_f
-    puts (t2 - t1).to_s + " seconds."
+    # t2 = Time.now
+    #    "%.9f" % t1.to_f
+    #    "%.9f" % t2.to_f
+    # puts (t2 - t1).to_s + " seconds."
 
     pathForSave.nil? ? blur.save(File.join(@picturePath, name)) : blur.save(File.join(pathForSave, name))
   end
@@ -206,13 +189,8 @@ class Image
   # this function makes use of an interesting matrix of trig functions in order to apply a unique filter to the image
   
   def surveillanceCamera(name = nil, pathForImage = nil, pathForSave = nil)
-    if name.nil? then
-      name = @pictureName.gsub('.png', '1984.png')
-    end
-    
-    if pathForImage.nil? then
-      pathForImage = File.join(@picturePath, @pictureName)
-    end
+    name ||= @pictureName.gsub('.png', '1984.png')
+    pathForImage ||= File.join(@picturePath, @pictureName)
 
     pi = Math::PI
     height = @height-3
@@ -225,7 +203,6 @@ class Image
           [-1*Math.sin(j*pi), -1*Math.sin(j*pi), -1*Math.sin(j*pi)], 
           [-1*Math.tan((i/j)*pi), -1*Math.tan((i/j)*pi), -1*Math.tan((i/j)*pi)]
           ], @picture, i, j, false)
-        # pixel = calculatePixelValueWithFilter3([[-1, -1, -1], [-1, 8, -1], [Math.tan(j/pi), Math.tan(j/pi), Math.tan(j/pi)]], @picture, i, j, false) # curtains filter
         blur[i, j] = ChunkyPNG::Color.rgb(pixel[0].to_i, pixel[1].to_i, pixel[2].to_i)
       end
     end
@@ -240,13 +217,8 @@ class Image
   # this function sharpens an image in order to highlight differences between edges
   
   def sharpen(name = nil, pathForImage = nil, pathForSave = nil)
-    if name.nil? then
-      name = @pictureName.gsub('.png', 'Sharpened.png')
-    end
-    
-    if pathForImage.nil? then
-      pathForImage = File.join(@picturePath, @pictureName)
-    end
+    name ||= @pictureName.gsub('.png', 'Sharpened.png')
+    pathForImage ||= File.join(@picturePath, @pictureName)
     
     sharpenFilter = [[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]
     
@@ -271,9 +243,7 @@ class Image
   # the user is allowed to click on points in the image which are then used as the control points for the bezier curve
   
   def bezierCurve(name = nil, pathForImage = nil, pathForSave = nil)
-    if name.nil? then
-      name = @pictureName.gsub('.png', 'Bezier.png')
-    end
+    name ||= @pictureName.gsub('.png', 'Bezier.png')
     
     displayPicture(pathForImage)
     points = parsePoints
@@ -349,7 +319,6 @@ class Image
   end
   
   ## PRIVATE UTILITY FUNCTIONS
-  
   private
   
   ## constrain function
